3c00a377860af103d6b910b38224e5e4b516ed7a
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-30 18:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+
+      document.addEventListener('DOMContentLoaded', function () {
     const statusParagraph = document.getElementById('status');
     const enterButton = document.getElementById('enterBtn');
 
     enterButton.addEventListener('click', function () {
-      // Create a new h1 element
-      const h1Element = document.createElement('h1');
-      
-      // Set the text content of the h1 element
-      h1Element.textContent = 'Entered Metaverse';
-
-      // Replace the existing p tag with the new h1 element
-      statusParagraph.replaceWith(h1Element);
+      // Change the text content of the paragraph
+      statusParagraph.textContent = 'Entered Metaverse';
     });
   });
+    
