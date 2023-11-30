9f7dfddc066751903f7d20351e27550439487ba8
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-30 18:26:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const statusParagraph = document.getElementById('status');
+    const enterButton = document.getElementById('enterBtn');
+
+    enterButton.addEventListener('click', function () {
+      // Change the text content of the paragraph
+      statusParagraph.textContent = 'Entered Metaverse';
+    });
+  });
